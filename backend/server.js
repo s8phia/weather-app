d4fd853215cb0ea9f79e6ba4689e3c71e4766f82
Sophia <sophiawg143@gmail.com>
2025-04-23 22:09:16 -0400

    connecting backend to frontend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+app.get("/api", (req, res) => {
+    return res.json({message: "This is from backend"});
+})
+app.listen(8081, () => {
+    console.log("listening");
+})
+
+
